9ed5710467bc173f9e8fb6d687fec88cb0bb245d
Marcin Bogdanski <marcin@bogdanski.info>
2018-04-14 16:37:21 +0200

    finished

---
+++ config.js
@@ -0,0 +1,5 @@
+module.export = {
+	'GOOGLE_CLIENT_ID':'664180050100-qn5sen520775hjv4edh7je11tsrssqb1.apps.googleusercontent.com',
+	'GOOGLE_CLIENT_SECRET':'6T8omgfHlGZWmJQf8qZ76Nm6',
+	'CALLBACK_URL':'http://localhost:3000/auth/google/callback'
+};
