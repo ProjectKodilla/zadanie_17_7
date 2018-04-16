d7636a942a07de9a370cff93594e8e3520531d16
Marcin <36379394+MarcinBB@users.noreply.github.com>
2018-04-16 10:09:29 +0200

    Update config.js

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
-module.export = {
+module.exports = {
 	'GOOGLE_CLIENT_ID':'664180050100-qn5sen520775hjv4edh7je11tsrssqb1.apps.googleusercontent.com',
 	'GOOGLE_CLIENT_SECRET':'6T8omgfHlGZWmJQf8qZ76Nm6',
 	'CALLBACK_URL':'http://localhost:3000/auth/google/callback'
